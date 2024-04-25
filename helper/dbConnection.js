2d2618de36c5134b7a7635657036aa474dec9948
Lokendra5947 <103491068+Lokendra5947@users.noreply.github.com>
2024-04-25 16:54:17 +0530

    Add files via upload

--- helper/dbConnection.js
+++ helper/dbConnection.js
@@ -1,5 +1,8 @@
 const { default: mongoose } = require("mongoose")
 
-mongoose.connect('mongodb://localhost:27017/IGTASK1')
+// mongoose.connect('mongodb://localhost:27017/IGTASK1')
+//   .then(() => console.log('Connected Successfully!!!')).catch((err)=>{console.log(err.message)})
+
+mongoose.connect('mongodb+srv://lokendrasinghr40:12344321@task1.6koylld.mongodb.net/IGTASK1')
   .then(() => console.log('Connected Successfully!!!')).catch((err)=>{console.log(err.message)})
   
